6baafb2815567bb1bfd4222908c2936a6178a72c
David <kochkin.david@gmail.com>
2023-07-07 21:25:40 +0300

    feat: Customization TasksTableViewCell

--- Schedule/Model/Cell/TasksTableViewCell.swift
+++ Schedule/Model/Cell/TasksTableViewCell.swift
@@ -8,19 +8,60 @@
 import UIKit
 
 class TasksTableViewCell: UITableViewCell {
+    
+    let taskName = UILabel(text: "Programming", font: .avenirNextDemiBold20())
+    let taskDescription = UILabel(text: "Learn how to write extension and apply them correctly ", font: .avenirNext14())
+    
+    let readyButton: UIButton = {
+       let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "chevron.down.circle"), for: .normal)
+        button.tintColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
        
         setConstraints()
         self.selectionStyle = .none
+        
+        taskDescription.numberOfLines = 2
+        
+        readyButton.addTarget(self, action: #selector(readyButtonTapped), for: .touchUpInside)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    @objc func readyButtonTapped() {
+        print("Tap")
+    }
+    
     func setConstraints() {
+        self.contentView.addSubview(readyButton)
+        NSLayoutConstraint.activate([
+            readyButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            readyButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            readyButton.heightAnchor.constraint(equalToConstant: 40),
+            readyButton.widthAnchor.constraint(equalToConstant: 40)
+        ])
         
+        self.addSubview(taskName)
+        NSLayoutConstraint.activate([
+            taskName.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+            taskName.trailingAnchor.constraint(equalTo: readyButton.leadingAnchor, constant: -5),
+            taskName.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 25),
+            taskName.heightAnchor.constraint(equalToConstant: 40),
+        ])
+        
+        self.addSubview(taskDescription)
+        NSLayoutConstraint.activate([
+            taskDescription.topAnchor.constraint(equalTo: taskName.bottomAnchor, constant: 5),
+            taskDescription.trailingAnchor.constraint(equalTo: readyButton.leadingAnchor, constant: -5),
+            taskDescription.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
+            taskDescription.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5)
+        ])
     }
 }
