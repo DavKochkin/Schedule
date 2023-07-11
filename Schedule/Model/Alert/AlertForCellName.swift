1753b5e6f94fc9b807f46dfc724c87a8fd3375ae
David <kochkin.david@gmail.com>
2023-07-11 13:42:38 +0300

    Add AlertController with TextField

---
+++ Schedule/Model/Alert/AlertForCellName.swift
@@ -0,0 +1,35 @@
+//
+//  AlertForCellName.swift
+//  Schedule
+//
+//  Created by David Kochkin on 11.07.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func alertForCellName(label: UILabel, name: String, placeholder: String) {
+        
+        let alert = UIAlertController(title: name, message: nil, preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "Ok", style: .default) { (action) in
+            
+            let tfAlert = alert.textFields?.first
+            guard let text = tfAlert?.text else { return}
+            label.text = text
+        }
+        
+        alert.addTextField { (tfAlert) in
+            tfAlert.placeholder = placeholder
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .default)
+        
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true)
+        
+    }
+}
