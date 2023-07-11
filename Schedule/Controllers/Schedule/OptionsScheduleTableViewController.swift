7f67eb87dbae68d567c58d0e756f5814dd6346a0
David <kochkin.david@gmail.com>
2023-07-11 13:22:30 +0300

    Add DatePicker for Time

--- Schedule/Controllers/Schedule/OptionsScheduleTableViewController.swift
+++ Schedule/Controllers/Schedule/OptionsScheduleTableViewController.swift
@@ -71,6 +71,9 @@ class OptionsScheduleTableViewController: UITableViewController {
         case [0,0]: alertDate(label: cell.nameCellLabel) { (numberWeekday, date) in
             print(numberWeekday, date)
         }
+        case [0,1]: alertTime(label: cell.nameCellLabel) { (date) in
+            print(date)
+        }
         default:
             print("ERROR")
         }
