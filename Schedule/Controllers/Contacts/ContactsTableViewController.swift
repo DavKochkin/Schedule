3f628c96c97de84ed9b361ade4241d31ded99b55
David <kochkin.david@gmail.com>
2023-07-12 12:59:33 +0300

    Add ContactsTableViewCell

--- Schedule/Controllers/Contacts/ContactsTableViewController.swift
+++ Schedule/Controllers/Contacts/ContactsTableViewController.swift
@@ -19,24 +19,30 @@ class ContactsTableViewController: UITableViewController {
         tableView.backgroundColor = .systemGray6
         tableView.separatorStyle = .singleLine
         tableView.bounces = false
-        tableView.register(OptionsTableViewCell.self, forCellReuseIdentifier: idContactsCell)
+        tableView.register(ContactsTableViewCell.self, forCellReuseIdentifier: idContactsCell)
         
         title = "Contacts"
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(AddButtonTapped))
     }
+    
+    @objc func AddButtonTapped() {
+        
+        let scheduleOption = OptionsScheduleTableViewController()
+        navigationController?.pushViewController(scheduleOption, animated: true)
+    } 
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: idContactsCell, for: indexPath) as! OptionsTableViewCell
-        cell.textLabel?.text = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: idContactsCell, for: indexPath) as! ContactsTableViewCell
         return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 44
+        return 80
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
