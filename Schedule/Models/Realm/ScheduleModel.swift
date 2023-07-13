72971474bbc65642128b93e45d732c5483c607d8
David <kochkin.david@gmail.com>
2023-07-13 12:44:30 +0300

    Add Realm Model

---
+++ Schedule/Models/Realm/ScheduleModel.swift
@@ -0,0 +1,23 @@
+//
+//  ScheduleModel.swift
+//  Schedule
+//
+//  Created by David Kochkin on 13.07.2023.
+//
+
+import RealmSwift
+import Foundation
+
+class ScheduleModel: Object {
+    
+    @Persisted var scheduleDate = Date()
+    @Persisted var scheduleTime = Date()
+    @Persisted var scheduleName: String = ""
+    @Persisted var scheduleType: String = ""
+    @Persisted var scheduleBuilding: String = ""
+    @Persisted var scheduleAudience: String = ""
+    @Persisted var scheduleTeacher: String = ""
+    @Persisted var scheduleColor: String = ""
+    @Persisted var scheduleRepeat: Bool = true
+    @Persisted var scheduleWeekday: Int = 1
+}
---
+++ Schedule/Services/RealmManager.swift
@@ -0,0 +1,8 @@
+//
+//  RealmManager.swift
+//  Schedule
+//
+//  Created by David Kochkin on 13.07.2023.
+//
+
+import Foundation
